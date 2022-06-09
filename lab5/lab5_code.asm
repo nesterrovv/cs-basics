f4f67eaec0856a14b8f6fc8d9469edd1e723d09d
Ivan Nesterov <71551187+nesterrovv@users.noreply.github.com>
2022-06-09 09:21:22 +0300

    comments in asm code translated to english

--- lab5/lab5_code.asm
+++ lab5/lab5_code.asm
@@ -1,34 +1,34 @@
-		ORG 0x584	;Адрес начала программы
-ADDR:		WORD $RES	;Ссылка на результат
-MEM:		NOP		;Ячейка для записи нечетных символов
-START:		CLA		;Очистка аккумулятора
-S1:		IN 0x5		;Ожидание ввода нечетного символа
-		AND #0x40	;Проверка на наличие введенного символа
-		BEQ S1		;Нет - "Спин-луп"
-		IN 0x4		;Вывод байта в AC
-		OUT 0x6		;Вывод байта в ВУ-3
-		ST (ADDR)	;Сохраняем символ в результат
-		ST $MEM		;Сохраняем символ в "кэш"
-		CMP #0x00	;Проверяем на стоп-символ
-		BEQ EXIT	;Если стоп-символ - выход
-		CLA		;Очистка аккумулятора
-S2:		IN 0x5		;Ожидание ввода четного символа
-		AND #0x40	;Проверка на наличие введенного символа
-		BEQ S2		;Нет - "Спин-луп"
-		IN 0x4		;Вывод байта в AC
-		OUT 0x6		;Вывод байта в ВУ-3
-		SWAB		;Перемещаем четный символ в старший байт
-		OR $MEM		;Совмещаем с 1-м символом
-		ST (ADDR)	;Сохраняем в память с автоинкрементом ссылки
-		SUB $MEM	;Вычитаем 1-й символ
-		SWAB		;Перемещаем четный символ в младший байт
-		CMP #0x00	;Проверяем на стоп-символ
-		BEQ EXIT	;Если стоп-символ - выход
-		LD (ADDR)+	;Инкрементируем ссылку на результат
-		CLA		;Очистка аккумулятора
-		JUMP S1		;Возвращаемся в начало цикла
-EXIT:		LD (ADDR)+	;Инкрементируем ссылку на результат
-		CLA		;Очистка аккумулятора
-		HLT		;Остановка программы
-		ORG 0x5AC	;Адрес начала хранения результата
+		ORG 0x584	;Program start address
+ADDR:		WORD $RES	;Link to result
+MEM:		NOP		;Cell to write odd characters
+START:		CLA		;Accumulator cleaning
+S1:		IN 0x5		;Waiting for odd character input
+		AND #0x40	;Check for the entered character
+		BEQ S1		;None - "Spin Loop"
+		IN 0x4		;Output byte to AC
+		OUT 0x6		;Output byte to ED-3
+		ST (ADDR)	;Save character to result
+		ST $MEM		;Save character to "cache"
+		CMP #0x00	;Check for a stop character
+		BEQ EXIT	;If stop character is an exit
+		CLA		;Accumulator cleaning
+S2:		IN 0x5		;Waiting for even character input
+		AND #0x40	;Check for the entered character
+		BEQ S2		;None - "Spin Loop"
+		IN 0x4		;Output byte to AC
+		OUT 0x6		;Output byte to ED-3
+		SWAB		Move even character to high byte
+		OR $MEM		;Match with 1st character
+		ST (ADDR)	;Save to memory with auto-increment links
+		SUB $MEM	;Subtract 1st character
+		SWAB		;Move even character to low byte
+		CMP #0x00	;Check for a stop character
+		BEQ EXIT	;If stop character is an exit
+		LD (ADDR)+	;Increment the link to the result
+		CLA		;Accumulator cleaning
+		JUMP S1		;Return to the beginning of the loop
+EXIT:		LD (ADDR)+	;Increment link to result
+		CLA		;Accumulator cleaning
+		HLT		;Stop program
+		ORG 0x5AC	;Address to start storing the result
 RES:		NOP
